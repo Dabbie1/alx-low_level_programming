fbfe43f5b0cfcf5d531183c7d2ec1c97166cabeb
Dabbie1 <dabelchimaks@gmail.com>
2022-10-18 08:53:46 -0700

    Task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar (j);
+	}
+	_putchar('\n');
+}
