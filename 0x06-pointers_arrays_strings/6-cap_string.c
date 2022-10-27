2788fa92e7c768bbaeb839fdeae211812d69586e
Dabbie1 <dabelchimaks@gmail.com>
2022-10-27 08:05:11 -0700

    Task 6 commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,31 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int x, y;
+	int trigger;
+	char nots[] = ",;.!?(){}\nt\" ";
 
-	while (str[i])
+	for (x = 0, trigger = 0; str[x] != '\0'; x++)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i = 1] == ';' ||
-				str[i = 1] == '.' ||
-				str[i = 1] == '!' ||
-				str[i = 1] == '?' ||
-				str[i = 1] == '"' ||
-				str[i = 1] == '(' ||
-				str[i = 1] == ')' ||
-				str[i = 1] == '{' ||
-				str[i = 1] == '}' ||
-				i = 0)
-			str[i] -= 32;
-		i++;
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (y = 0; nots[y] != '\0'; y++)
+		{
+			if (nots[y] == str[x])
+				trigger = 1;
+		}
+		if (trigger)
+		{
+			if (str[x] > 96 && str[x] < 123)
+			{
+				str[x] -= 32;
+				trigger = 0;
+			}
+			else if (str[x] > 64 && str[x] < 91)
+				trigger = 0;
+			else if (str[x] > 47 && str[x] < 58)
+				trigger = 0;
+		}
 	}
 	return (str);
 }
